3f46f4184883a7d3e7974cee1d318a0e34c7cd1f
Serguei Cambour <s.cambour@gmail.com>
2022-02-10 10:51:33 +0100

    Add posts creation in seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,28 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias BlogApi.Blog.Post
+alias BlogApi.{Blog, Repo}
+
+Faker.start()
+
+Repo.delete_all(Post)
+
+posts = [
+  %{
+    title: Faker.Lorem.sentence,
+    content: Faker.Lorem.paragraph
+  },
+  %{
+    title: Faker.Lorem.sentence,
+    content: Faker.Lorem.paragraph
+  },
+  %{
+    title: Faker.Lorem.sentence,
+    content: Faker.Lorem.paragraph
+  }
+]
+
+Enum.each(posts, fn post ->
+  Blog.create_post(post)
+end)
